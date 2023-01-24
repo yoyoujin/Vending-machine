30f3384a599caf8c0ca1cf21c49c6e1a3c3801a1
youjin <yjinhann@gmail.com>
2023-01-24 23:28:04 +0900

    test: 콜라 이미지 경로 수정

--- js/cola.js
+++ js/cola.js
@@ -1,37 +1,37 @@
 const colaData = [
   {
     name: 'Original_Cola',
-    img: '../../images/Original_Cola.svg',
+    img: '../images/Original_Cola.svg',
     price: 1000,
     stock: 5,
   },
   {
     name: 'Violet_Cola',
-    img: '../../images/Violet_Cola.svg',
+    img: '../images/Violet_Cola.svg',
     price: 1000,
     stock: 5,
   },
   {
     name: 'Yellow_Cola',
-    img: '../../images/Yellow_Cola.svg',
+    img: '../images/Yellow_Cola.svg',
     price: 1000,
     stock: 5,
   },
   {
     name: 'Cool_Cola',
-    img: '../../images/Cool_Cola.svg',
+    img: '../images/Cool_Cola.svg',
     price: 1000,
     stock: 5,
   },
   {
     name: 'Green_Cola',
-    img: '../../images/Green_Cola.svg',
+    img: '../images/Green_Cola.svg',
     price: 1000,
     stock: 5,
   },
   {
     name: 'Orange_Cola',
-    img: '../../images/Orange_Cola.svg',
+    img: '../images/Orange_Cola.svg',
     price: 1000,
     stock: 5,
   },
